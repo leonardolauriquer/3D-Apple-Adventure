956402bc7f5108c7eb009ecb7d3bb65674139c03
Leonardo Lauriquer Ribeiro <53185821+leonardolauriquer@users.noreply.github.com>
2025-10-26 00:18:55 -0300

    feat: Configure Capacitor for native builds
    Integrates Capacitor into the project to enable native builds for mobile platforms. Adds the `@capacitor/cli` dependency and configures the `capacitor.config.ts` file with essential project details. Updates `index.html` and `package.json` to reflect the application name change.

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.apple.adventure3d',
+  appName: '3D Apple Adventure',
+  webDir: '.',
+  bundledWebRuntime: false
+};
+
+export default config;
